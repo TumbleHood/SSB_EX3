70c0d5aae52ce5632de9fbbd4eead8b44cec9954
TumbleHood <guyga8@gmail.com>
2022-04-04 19:56:46 +0300

    finished headers

---
+++ Matrix.hpp
@@ -0,0 +1,62 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+namespace zich{
+    class Matrix{
+        public:
+            vector<double> _matrix;
+            int _width;
+            int _height;
+
+            Matrix(vector<double>& values, int width, int height);
+
+            Matrix operator+();
+            Matrix operator-();
+
+            Matrix operator+(const int);
+            Matrix operator-(const int);
+            Matrix operator*(const int);
+
+            Matrix operator+(const double);
+            Matrix operator-(const double);
+            Matrix operator*(const double);
+
+            Matrix operator+(const Matrix&);
+            Matrix operator-(const Matrix&);
+            Matrix operator*(const Matrix&);
+
+            Matrix operator+=(const int);
+            Matrix operator-=(const int);
+            Matrix operator*=(const int);
+
+            Matrix operator+=(const double);
+            Matrix operator-=(const double);
+            Matrix operator*=(const double);
+
+            Matrix operator+=(const Matrix&);
+            Matrix operator-=(const Matrix&);
+            Matrix operator*=(const Matrix&);
+
+            Matrix& operator=(const Matrix&);
+
+            bool operator==(const Matrix&);
+            bool operator!=(const Matrix&);
+            bool operator>(const Matrix&);
+            bool operator<(const Matrix&);
+            bool operator>=(const Matrix&);
+            bool operator<=(const Matrix&);
+            
+            friend ostream& operator<<(ostream&, const Matrix&);
+            friend istream& operator>>(istream&, const Matrix&);
+
+            friend Matrix operator+(const int, Matrix&);
+            friend Matrix operator-(const int, Matrix&);
+            friend Matrix operator*(const int, Matrix&);
+
+            friend Matrix operator+(const double, Matrix&);
+            friend Matrix operator-(const double, Matrix&);
+            friend Matrix operator*(const double, Matrix&);
+    };
+}
